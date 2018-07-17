7364310fb71b4cc77a59b0167e03f577fadc6811
Nate Iler <nate@flipboxdigital.com>
2018-07-17 12:22:26 -0600

    Adding salesforce icon for Patron

--- src/patron/Events.php
+++ src/patron/Events.php
@@ -4,6 +4,7 @@ namespace flipbox\force\patron;
 
 use flipbox\force\patron\providers\SalesforceSettings;
 use flipbox\patron\cp\Cp;
+use flipbox\patron\events\RegisterProviderIcon;
 use flipbox\patron\events\RegisterProviders;
 use flipbox\patron\events\RegisterProviderSettings;
 use Stevenmaguire\OAuth2\Client\Provider\Salesforce as SalesforceProvider;
@@ -31,6 +32,15 @@ class Events
             RegisterProviderSettings::REGISTER_SETTINGS,
             function (RegisterProviderSettings $event) {
                 $event->class = SalesforceSettings::class;
+            }
+        );
+
+        // OAuth2 Provider Icon
+        RegisterProviderIcon::on(
+            SalesforceProvider::class,
+            RegisterProviderIcon::REGISTER_ICON,
+            function (RegisterProviderIcon $event) {
+                $event->icon = '@vendor/flipboxfactory/force/src/icons/salesforce.svg';
             }
         );
     }
